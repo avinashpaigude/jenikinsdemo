a59f82005fb2286290e8b0c568010e533eef3c3f
avinashpaigude <37294823+avinashpaigude@users.noreply.github.com>
2018-04-08 01:14:38 +0530

    updated the file

--- src/def/smoketest.java
+++ src/def/smoketest.java
@@ -25,6 +25,7 @@ public class smoketest
 		driver.findElement(By.id("email")).sendKeys("avinashpaigude@gmail.com");
 		driver.findElement(By.id("pass")).sendKeys("sfsfsf");
 		driver.close();
+		System.out.println("this is the  new change")
 	
 	}
 
